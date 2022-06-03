8d43bdcae3553f295e5fddb0056a30b542fcb932
RAV079 <106539086+RAV079@users.noreply.github.com>
2022-06-03 14:24:19 -0500

    Add files via upload

---
+++ Presentation using contacts 04:10.py
@@ -0,0 +1,8 @@
+nombre='rodrigo'
+print('My name is:',nombre)
+grado='9C'
+print('My grade and group is:',grado)
+comida='Sushi'
+print('My favorite food is:',comida)
+estado='Tired'
+print('I feel:',estado)
