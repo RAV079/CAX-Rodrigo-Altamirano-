c194474dce4e05fcc509b03f6d729ea8854a0367
RAV079 <106539086+RAV079@users.noreply.github.com>
2022-06-30 10:47:42 -0500

    Add files via upload

---
+++ Python/2T/2T, 1T.py
@@ -0,0 +1,8 @@
+print('Welcome to Super Servicio Vista')
+print('What do you want to buy today?')
+numero1=(input('enter your article:'))
+numero2=int(input('enter the price:'))
+numero3=(input('enter your article:'))
+numero4=int(input('enter the price:'))
+resultado=numero2+numero4
+print('the result is',resultado)
